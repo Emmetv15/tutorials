e33504b2fdc09d311e47fcef0095c2a3a101efb7
Abdullah Noman (STUDENT) <s294151@student.salfordcc.ac.uk>
2022-01-12 14:55:59 +0000

    added sprite thing

--- render images/window.py
+++ render images/window.py
@@ -1,4 +1,6 @@
 import pygame
+import os
+
 pygame.init()
 
 screen = pygame.display.set_mode([512, 512])
@@ -10,7 +12,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    img = pygame.image.load(os.path.join(os.path.dirname(__file__), 'data/red.png'))
+
     screen.fill((18, 18, 18))
+    screen.blit(img, pygame.Rect(100, 100, 100, 100))
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
     pygame.display.flip()
 
