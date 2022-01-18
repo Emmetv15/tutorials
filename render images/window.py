5f7e7a6d0e0076c8cdf05d3ceea384107df37d65
Bartlomiej Wierzba (STUDENT) <s296577@student.salfordcc.ac.uk>
2022-01-18 11:53:13 +0000

    Small Changes

--- render images/window.py
+++ render images/window.py
@@ -1,16 +1,12 @@
 ## base window 
 
 import pygame
-<<<<<<< HEAD:render images/window.py
 import os
 
-=======
 from spriteandmovement import *
->>>>>>> 9205c676cfe1bb8d8c4afd88de4727cd3c02e3a1:window.py
 pygame.init()
 
 screen = pygame.display.set_mode([512, 512])
-
 running = True
 while running:
 
